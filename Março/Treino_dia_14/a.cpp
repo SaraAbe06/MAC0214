f81729b9e0c75641e084922391a5549998cb2350
SaraAbe06 <saramkabe@usp.br>
2026-03-19 11:12:55 -0300

    Create a.cpp

---
+++ Março/Treino_dia_14/a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll; 
+
+double pi=2*acos(0);
+double d;
+
+int main(){
+    cin>>d;
+    cout<<fixed<<setprecision(10);
+    cout<<pi*((double)d/2.0)*((double)d/2.0);
+}
