02d7b73209c65fbcf96e32c1c7efdd8b25630533
SaraAbe06 <saramkabe@usp.br>
2026-04-18 12:02:50 -0300

    Create a.cpp

---
+++ Abril/Treino_dia_02/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+
+    string s;cin>>s;
+    cout<<s;
+    for(int i=s.size()-1;i>=0;i--)cout<<s[i];
+}
